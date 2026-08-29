e2d8ca01f3ee6b5e22ccc2cc6cd39b476483026b
auato <75980629+auato@users.noreply.github.com>
2026-08-29 11:52:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = "mylist-v16";
+const NOME_CACHE = "mylist-v17";
 const FILE_DA_METTERE_IN_CACHE = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", evento => {
